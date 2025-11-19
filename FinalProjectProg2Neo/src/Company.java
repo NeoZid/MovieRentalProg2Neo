2e00355ac4a4dd14146034f6e25ddb09376ec3fc
frapp <frapp@10.16.106.161>
2025-11-19 11:59:19 -0500

    ExternalMember class

--- FinalProjectProg2Neo/src/Company.java
+++ FinalProjectProg2Neo/src/Company.java
@@ -3,6 +3,7 @@ public class Company {
 	int ID;
 	String companyName;
 	boolean isActive;
+	
 	public Company(int ID, String companyName) {
 		this.ID=ID;
 		this.companyName=companyName;
---
+++ FinalProjectProg2Neo/src/ExternalMember.java
@@ -0,0 +1,11 @@
+
+public class ExternalMember {
+	String job;
+	String organization;
+	
+	
+	public ExternalMember(String job, organization) {
+		this.job=job;
+		this.organization=organization;
+	}
+}
