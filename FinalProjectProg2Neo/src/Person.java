d9798244d00bf9acabd65b82523e770bc9b42f6e
NeoZid <zidereckn06@gmail.com>
2025-11-18 21:30:57 -0500

    Person, RentalService, Company, Student, Maindriver classes added. Test code on main driver

--- FinalProjectProg2Neo/src/Person.java
+++ FinalProjectProg2Neo/src/Person.java
@@ -1,8 +1,14 @@
 
 public class Person {
-	String personID;
+	int personID;
 	String name;
 	int age;
 	String gender;
-
+	public Person(int personID, String name, int age, String gender) {
+		this.personID=personID;
+		this.name=name;
+		this.age=age;
+		this.gender=gender;
+	}
+	
 }
